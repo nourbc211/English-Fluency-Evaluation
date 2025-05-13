12d80be54e076e9043848639244df6e9e3d02b96
nourbc211 <149681819+nourbc211@users.noreply.github.com>
2025-05-13 12:57:24 +0100

    Update feature_extractor.py to avoid spacy issues

--- Evaluator/pipeline/feature_extractor.py
+++ Evaluator/pipeline/feature_extractor.py
@@ -26,7 +26,14 @@ def load_transcript(audio_path, audio_base_dir='input/segments/', transcript_bas
 
 # --- Text feature extraction ---
 
-nlp = spacy.load("en_core_web_sm")
+# nlp = spacy.load("en_core_web_sm")
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 def extract_text_features(text):
     """
