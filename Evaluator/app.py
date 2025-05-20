d13561ec9c427a0e09b639d79f6c401d737feb5a
nourbc211 <149681819+nourbc211@users.noreply.github.com>
2025-05-20 12:41:10 +0100

    bug

--- Evaluator/app.py
+++ Evaluator/app.py
@@ -1,15 +1,10 @@
-# Adding this to avoid crashes due to torchaudio
-import asyncio
-try:
-    asyncio.get_running_loop()
-except RuntimeError:
-    asyncio.set_event_loop(asyncio.new_event_loop())
-
 import streamlit as st
 import os
 import shutil
 import io
 import torch
+torch.classes.__path__ = [] # to avoid crashes in streamlit
+
 import torchaudio
 import soundfile as sf
 
