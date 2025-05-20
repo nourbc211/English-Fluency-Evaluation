604191c42b82095f471ff0afc51ae8f98117243a
nourbc211 <149681819+nourbc211@users.noreply.github.com>
2025-05-20 12:35:27 +0100

    fixed torchaudio related issue

--- Evaluator/app.py
+++ Evaluator/app.py
@@ -1,3 +1,10 @@
+# Adding this to avoid crashes due to torchaudio
+import asyncio
+try:
+    asyncio.get_running_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop())
+
 import streamlit as st
 import os
 import shutil
